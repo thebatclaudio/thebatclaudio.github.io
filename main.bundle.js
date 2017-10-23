79bbc064698cadb5770528f402878b95d4fc0bfb
Claudio La Barbera <labarbera.claudio@gmail.com>
2017-10-23 13:06:18 +0200

    Edited particle.js configuration

--- main.bundle.js
+++ main.bundle.js
@@ -148,7 +148,19 @@ var AppComponent = (function () {
                     "value": "#e86f6f"
                 },
                 "shape": {
-                    "type": "circle"
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
                 },
                 "opacity": {
                     "value": 0.5,
@@ -163,6 +175,12 @@ var AppComponent = (function () {
                 "size": {
                     "value": 3,
                     "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
                 },
                 "line_linked": {
                     "enable": false,
@@ -183,6 +201,45 @@ var AppComponent = (function () {
                         "enable": false,
                         "rotateX": 600,
                         "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": false,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200,
+                        "duration": 0.4
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
                     }
                 }
             },
